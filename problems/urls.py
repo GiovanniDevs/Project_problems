1515d988f4d3f96788c180ff4a82dc2280862a89
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-02-11 13:09:05 +0000

    feat: :sparkles: add problem detail view

--- problems/urls.py
+++ problems/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ProblemList.as_view(), name='home'),
+    path('<slug:slug>/', views.problem_detail, name='problem_detail'),
 ]
--- problems/views.py
+++ problems/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Problem
 
@@ -9,3 +9,27 @@ class ProblemList(generic.ListView):
     queryset = Problem.objects.filter(status='public')
     template_name = "problems/index.html"
     paginate_by = 6
+
+
+def problem_detail(request, slug):
+    """
+    Display an individual :model:`problems.Problem`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`problems.Problem`.
+
+    **Template:**
+
+    :template:`problems/problem_detail.html`
+    """
+
+    queryset = Problem.objects.filter(status='public')
+    problem = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "problems/problem_detail.html",
+        {"problem": problem},
+    )
