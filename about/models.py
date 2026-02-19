5fd84505b77066e20493d3d61dcf2deb35a36029
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-02-19 09:28:18 +0000

    style: :lipstick: add cloudinary image to about page
    also adjust some styling choices

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class About(models.Model):
     title = models.CharField(max_length=200)
     updated_date = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    about_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.title
