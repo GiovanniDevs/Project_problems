36e05175e281dc7f864e8d88f2074a287ecc3cbd
GiovanniDevs <damico.giovanni.90@gmail.com>
2026-02-23 16:28:32 +0000

    fix: :bug: rebuild static files

---
+++ staticfiles/js/takes.js
@@ -0,0 +1,56 @@
+const editButtons = document.getElementsByClassName("btn-edit");
+const takeText = document.getElementById("id_description");
+const takePain = document.getElementById("id_pain_level");
+const takeAffected = document.getElementById("id_affected_people");
+const takeFrequency = document.getElementById("id_frequency");
+const takeForm = document.getElementById("takeForm");
+const submitButton = document.getElementById("submitButton");
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+/*
+ * Initializes edit functionality for the provided edit buttons.
+ *
+ * For each button in the `editButtons` collection:
+ * - Retrieves the associated comment's ID upon click.
+ * - Fetches the content of the corresponding comment.
+ * - Populates the `commentText` input/textarea with the comment's content for editing.
+ * - Updates the submit button's text to "Update".
+ * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+ */
+
+for (let button of editButtons) {
+  button.addEventListener("click", (e) => {
+    let takeId = e.target.getAttribute("data-take_id");
+    let takeContent = document.getElementById(`take${takeId}`).innerText;
+    let getPain = document.getElementById(`pain${takeId}`).innerText;
+    let getFrequency = document.getElementById(`freq${takeId}`).innerText;
+    let getAffected = document.getElementById(`affected${takeId}`).innerText;
+    takeText.value = takeContent;
+    takePain.value = getPain;
+    takeFrequency.value = getFrequency;
+    takeAffected.value = getAffected;
+    submitButton.innerText = "Update";
+    takeForm.setAttribute("action", `edit_take/${takeId}`);
+  });
+}
+
+/*
+ * Initializes deletion functionality for the provided delete buttons.
+ *
+ * For each button in the `deleteButtons` collection:
+ * - Retrieves the associated comment's ID upon click.
+ * - Updates the `deleteConfirm` link's href to point to the
+ * deletion endpoint for the specific comment.
+ * - Displays a confirmation modal (`deleteModal`) to prompt
+ * the user for confirmation before deletion.
+ */
+for (let button of deleteButtons) {
+  button.addEventListener("click", (e) => {
+    let takeId = e.target.getAttribute("data-take_id");
+    deleteConfirm.href = `delete_take/${takeId}`;
+    deleteModal.show();
+  });
+}
